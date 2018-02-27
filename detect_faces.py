b435775dc1544f3934efc843a8b8ad3a377d51cf
0☠DECEA5ED <0xDECEA5ED@users.noreply.github.com>
2018-02-27 17:18:51 -0600

    Update detect_faces.py

--- detect_faces.py
+++ detect_faces.py
@@ -7,28 +7,29 @@ import numpy as np
 
 #	Construct an argument parser
 ap = argparse.ArgumentParser()
-#	Creates args -i and requires it for operation
+#	Creates our args
 ap.add_argument("-i", "--image", required=True, help = "path to the image to be parsed")
+ap.add_argument("-c", "--cascade", required=True, help = "path to the face cascade")
+ap.add_argument("-e", "--eyes", help = "include this flag to scan faces for eyes", action="store_true")
 #	Parse our args
 args = vars(ap.parse_args())
-
+#	Process our image
 image = cv2.imread(args["image"])
-faceCascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+faceCascade = cv2.CascadeClassifier(args["cascade"])
 eyeCascade = cv2.CascadeClassifier('cascades/haarcascade_eye.xml')
 grayedImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#	Create our FaceDetector and scan image for faces
 faceD = FaceDetector()
 faces = faceD.detectFace(grayedImage, faceCascade)
-
-print("Found {0} faces!".format(len(faces)))
-
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
 	cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), int(w/100) + 2)
-	eyes = faceD.detectEyes(grayedImage[y:y+h, x:x+w], eyeCascade)
-	for (ex, ey, ew, eh) in eyes:
-		cv2.rectangle(image[y:y+h, x:x+w], (ex, ey), (ex+ew, ey+eh), (255, 0, 0), int(ew/100) + 1)
-
+	#	If the --eyes flag is included, scan for eyes within the face rectangles
+	if args["eyes"]:
+		eyes = faceD.detectEyes(grayedImage[y:y+h, x:x+w], eyeCascade)
+		for (ex, ey, ew, eh) in eyes:
+			cv2.rectangle(image[y:y+h, x:x+w], (ex, ey), (ex+ew, ey+eh), (255, 0, 0), int(ew/100) + 1)
 #	Display the image
-cv2.imshow("Faces found", image)
+cv2.imshow("Faces found: " + str(len(faces)), image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
